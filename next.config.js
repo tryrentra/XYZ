54ac2f069840ef30ebb05c212e9295e7cb1ddfc6
tryrentra <128274036+tryrentra@users.noreply.github.com>
2023-03-27 15:11:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports={env: {
     NEXT_PUBLIC_API_URL: 'https://gamynt.com/',
-    NEXT_PUBLIC_SOCKET: 'https://gamynt.com:4000/'
-  },
-  server: {
-    https: false
+    NEXT_PUBLIC_SOCKET: 'https://gamynt.com/'
   }
 }
