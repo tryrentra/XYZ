63ed51c34e8df53fa55683d3d58c2e5f9e4b9f65
jicro <126085710+jicrogithub@users.noreply.github.com>
2023-03-27 08:41:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-env: {
+module.exports={env: {
     NEXT_PUBLIC_API_URL: 'http://68.183.85.63:8080/',
     NEXT_PUBLIC_SOCKET: 'http://68.183.85.63:4000/'
   },
   server: {
     https: false
   }
-
-module.exports = nextConfig
+}
