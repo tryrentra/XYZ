83b4a12c692890ef3a72e1b39ae78ef0f01538b8
tryrentra <128274036+tryrentra@users.noreply.github.com>
2023-03-28 06:48:07 +0530

    Update ChatSide.jsx

--- component/chat/ChatSide.jsx
+++ component/chat/ChatSide.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import io from "socket.io-client";
 import style from "./styles/chatside.module.scss";
 import { useRouter } from "next/router"
 const socket = io(process.env.NEXT_PUBLIC_SOCKET, {
   rejectUnauthorized: false
-}
-);
+});
 import axios from "axios"
+
 export default function ChatSide() {
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState("");
@@ -14,17 +14,19 @@ export default function ChatSide() {
   let api = process.env.NEXT_PUBLIC_API_URL
 
   const router = useRouter()
+
   useEffect(() => {
     axios.get(`${api}api/chats`).then((e) => {
       setMessages([...messages, e.data.chats.flat(Infinity)].flat(Infinity));
     })
   }, []);
-  console.log(messages)
+
   useEffect(() => {
     const id = localStorage.getItem("id")
     !id && router.push("/")
     setID(id)
   }, [])
+
   useEffect(() => {
     socket.on("message", (message) => {
       setMessages([...messages, message]);
@@ -40,9 +42,18 @@ export default function ChatSide() {
   }
 
   const messagesEndRef = useRef(null);
+
   useEffect(() => {
     messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
+
+  // memoize the messages state for caching
+  const memoizedMessages = useMemo(() => messages, [messages]);
+
+  // cache the messages in localStorage
+  useEffect(() => {
+    localStorage.setItem("messages", JSON.stringify(memoizedMessages));
+  }, [memoizedMessages]);
 
   return (
     <div className={style.chatSideMain}>
@@ -68,7 +79,7 @@ export default function ChatSide() {
           />
           <button onClick={() => {
             const timestamp = new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })
-            socket.emit("message", { text, id,timestamp });
+            socket.emit("message", { text, id, timestamp });
             setText("");
           }} className="input-box__button">Send</button>
         </div>
