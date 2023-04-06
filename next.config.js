1d4559f917bd7dfb1c04fc4f0a166f266056c465
tryrentra <128274036+tryrentra@users.noreply.github.com>
2023-04-06 07:43:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports={env: {
-    NEXT_PUBLIC_API_URL: 'https://gamynt.com/',
-    NEXT_PUBLIC_SOCKET: 'https://gamynt.com/'
+    NEXT_PUBLIC_API_URL: 'https://api-chatto.onrender.com/',
+    NEXT_PUBLIC_SOCKET: 'https://api-chatto.onrender.com/'
   }
 }
