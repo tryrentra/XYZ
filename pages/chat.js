a76a971bfe084809d46d411e2885f1d5a8098340
tryrentra <128274036+tryrentra@users.noreply.github.com>
2023-04-06 07:49:24 +0530

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -3,9 +3,11 @@ import MemberSide from '@/component/chat/MemberSide'
 import React, { useState, useEffect } from 'react';
 import style from "./styles/chat.module.scss"
 const chat = () => {
+  const [menustate, setmenustate] = useState(false)
   return (
-    <div className={style.chatContainer}>
+    <div className={`${style.chatContainer}  ${menustate == true && style.responsive}`}>
 <div className={style.memberSide}>
+  <button className={style.menubtn} onClick={()=>{setmenustate(menustate == false ? true : false)}}>{menustate == false ? "close" : "groups"}</button>
   <MemberSide/>
 </div>
 <div className={style.chatSide}>
@@ -13,6 +15,7 @@ const chat = () => {
 </div>
     </div>
   )
+
 }
 
 export default chat
