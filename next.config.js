953524507ff70da915b744252e51bf465d8ddced
tryrentra <128274036+tryrentra@users.noreply.github.com>
2023-04-07 13:21:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports={env: {
-    NEXT_PUBLIC_API_URL: 'https://chatto-production.up.railway.app/',
-    NEXT_PUBLIC_SOCKET: 'https://chatto-production.up.railway.app/'
+    NEXT_PUBLIC_API_URL: 'https://chatto-api-tn8rk.ondigitalocean.app/',
+    NEXT_PUBLIC_SOCKET: 'https://chatto-api-tn8rk.ondigitalocean.app/'
   }
 }
